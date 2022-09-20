ced4ab43bba7106f3bd699d9cd591a6761704a69
SuryaJustin <113465869+SuryaJustin@users.noreply.github.com>
2022-09-20 14:16:35 +0530

    Update TShirtCompany.java

--- src/Day2/TShirtCompany.java
+++ src/Day2/TShirtCompany.java
@@ -1,18 +1,29 @@
 package Day2;
 
+// Class contains variables, method, main method, object
+//class has the ability to access all the data members (dara members and mthods) of the main class uncluding privte
+//we declare the class as final class(){} we can't inherit
+
+//Object gives memory allocation
+//Object is a single instance of a class
 
 import java.util.Scanner;
 
 public class TShirtCompany {
 
-    static {
+    //static block exeute first when we run the program
+    //we declare publi static --> we call the method as global
+    static { 
         System.out.println("Welcome to TShirts shop");
     }
 
     public void Customers() {
         int TShirt = 50;
 
-        for (int i = 0; i <TShirt; ) {
+        //For loop is repeating actoin
+        //For loop travel both forward and backward direction
+        //For loop is condition based one
+        for (int i = 0; i <TShirt; ) {  
             Scanner sc = new Scanner(System.in);
             int count = sc.nextInt();
             String colour = sc.next();
